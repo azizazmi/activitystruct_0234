86d8f2d27df2c055fc30eaceefb2558f684ee3b1
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-04-02 15:18:24 +0700

    menambahkan structure DetailAlamat dan Mahasiswa dari contoh kedua

--- strukturE_3.cpp
+++ strukturE_3.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
