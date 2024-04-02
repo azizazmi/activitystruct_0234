5044b48d57fcb62df6307dbbfa543b2fa43e5457
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-04-02 15:48:16 +0700

    menambahkan for untuk bagian input pada contoh keempat

--- strukturE_4.cpp
+++ strukturE_4.cpp
@@ -15,19 +15,21 @@ struct Mahasiswa {
 
 int main() {
     Mahasiswa mhs[3];
-    cout << "Nomor Mahasiswa: ";
-    getline(cin , mhs.nim);
-    cout << "Nama Mahasiswa: ";
-    getline(cin,mhs.nama);
+    for (int i = 0; i < 3; i++) {
+        cout << "Nomor Mahasiswa: ";
+        getline(cin , mhs.nim);
+        cout << "Nama Mahasiswa: ";
+        getline(cin,mhs.nama);
 
-    cout << "Alamat Mahasiswa: " << endl;
-    cout << "\t Nama Desa: ";
-    cin >> mhs.alamat.desa;
-    cout << "\t Nama Kota: ";
-    cin >> mhs.alamat.kota;
+        cout << "Alamat Mahasiswa: " << endl;
+        cout << "\t Nama Desa: ";
+        cin >> mhs.alamat.desa;
+        cout << "\t Nama Kota: ";
+        cin >> mhs.alamat.kota;
 
-    cout << "Umur Mahasiswa: ";
-    cin >> mhs.umur;
+        cout << "Umur Mahasiswa: ";
+        cin >> mhs.umur;
+    }
 
     cout << endl;
     cout << "\n NIM: " << mhs.nim;
